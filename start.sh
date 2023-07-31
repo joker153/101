e00f223a861536836c67164c5c78cf2ee37dadeb
joker153 <77005994+joker153@users.noreply.github.com>
2023-07-31 14:35:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/AMALPRO/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
+git clone https://github.com/joker153/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
 cd /Shortner-Converter-Bot-V2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
