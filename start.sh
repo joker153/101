f9861e10f22e95aea6d837288dc6fcd8a2248e56
joker153 <77005994+joker153@users.noreply.github.com>
2023-07-31 14:38:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/joker153/Shortner-Converter-Bot-V2.git /Shortner-Co
 cd /Shortner-Converter-Bot-V2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python3 
